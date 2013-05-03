0b571ec4c8a31948a2b302a750a1be8ef680a33d
danielps <danieljfa@gmail.com>
2013-05-03 15:15:21 -0300

    sales-client
    O projeto de vendas do livro, que veio do base-app-client

---
+++ sales-client/js/app.js
@@ -0,0 +1,13 @@
+$app = angular.module('app',['ngResource']);
+
+$app.config(function($routeProvider){
+	$routeProvider.
+	when('/',{templateUrl:'view/main.html'}).
+	when('/clientes/',{templateUrl:'view/clientes/main.html'}).
+
+	otherwise({redirectTo:'/'});
+});
+
+$app.run(function($rootScope){
+	$rootScope.rootServerUrl = "http://localhost/";
+});
