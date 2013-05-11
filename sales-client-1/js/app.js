6c402b50bae0b3449ad89e6d975e7982609a1d1c
danielps <danieljfa@gmail.com>
2013-05-11 10:06:17 -0300

    Comentários + método para paginação do ng-repeat

--- sales-client-1/js/app.js
+++ sales-client-1/js/app.js
@@ -1,15 +1,20 @@
 
+//URL de acesso ao servidor RESTful
 SERVER_URL = "http://localhost/sales-server";
 
+//Criação ao $app que é o modulo que representa toda a aplicação
 $app = angular.module('app',['ngResource']);
 
 $app.config(function($routeProvider,$httpProvider){
+
+	//Configura o route provider
 	$routeProvider.
 	when('/',{templateUrl:'view/main.html'}).
 	when('/clientes',{templateUrl:'view/clientes/main.html',controller:clientesController}).
 	otherwise({redirectTo:'/'});
 
-	//RESPONSE interceptor
+	//configura o RESPONSE interceptor, usado para exibir o ícone de acesso ao servidor
+	// e a exibir uma mensagem de erro caso o servidor retorne algum erro
 	$httpProvider.responseInterceptors.push(function($q,$rootScope) {
 		return function(promise) {
 			//Always disable loader
@@ -30,8 +35,6 @@ $app.config(function($routeProvider,$httpProvider){
 			      	else
 			      		alert("ERROR! See log console");
 			      }
-
-
 			      return $q.reject(response);
 			  });
 		}
@@ -40,21 +43,33 @@ $app.config(function($routeProvider,$httpProvider){
 
 $app.run(function($rootScope){
 
-	
-
+	//Uma flag que define se o ícone de acesso ao servidor deve estar ativado
 	$rootScope.showLoaderFlag = false;
 	
+	//Força que o ícone de acesso ao servidor seja ativado
 	$rootScope.showLoader=function(){
 		$rootScope.showLoaderFlag=true;
 	}
+	//Força que o ícone de acesso ao servidor seja desativado
 	$rootScope.hideLoader=function(){
 		$rootScope.showLoaderFlag=false;
 	}
 
+	//Método que retorna a URL completa de acesso ao servidor. 
+	// Evita usar concatena~]ap
 	$rootScope.server=function(url){
 		return SERVER_URL + url;
 	}
 
 });
 
-
+//We already have a limitTo filter built-in to angular,
+//let's make a startFrom filter
+$app.filter('startFrom', function() {
+	return function(input, start) {
+		if (input==null)
+			return null;
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
