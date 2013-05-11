eb8f88c7fa07f05851bacf77968564a0b2b2ed8e
danielps <danieljfa@gmail.com>
2013-05-11 10:06:26 -0300

    Paginação simples

--- sales-client-1/js/clientesController.js
+++ sales-client-1/js/clientesController.js
@@ -4,16 +4,25 @@ function clientesController($scope,$http)
 {
 	$scope.rows = null;
 
+	//Pagination
+	$scope.currentPage = 0;
+	$scope.pageSize = 15;
+
+	$scope.numberOfPages =function(){
+		return Math.ceil($scope.rows.length/$scope.pageSize);                
+	}
+
 	$scope.init = function(){
 		$scope.loadAll();
 	}
 
 	$scope.loadAll = function(){
-		$scope.showLoader();
 		$http.get($scope.server("/customers")).success(function(data){
 			$scope.rows = data;			
 		});
 	}
+
+
 
 	$scope.init();
 }
