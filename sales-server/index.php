7b00d53da904a13c38c22131bbbe991d8798da70
danielps <danieljfa@gmail.com>
2013-06-04 11:47:19 -0300

    Formatação

--- sales-server/index.php
+++ sales-server/index.php
@@ -21,30 +21,8 @@ function formatJson($obj)
     echo json_encode($obj);
 }
 
+//Includes
 include("customer.php");
 
-
-
-
-//GET pode possuir um parametro na URL
-/*
-$app->get('/:controller/:action(/:parameter)', function ($controller, $action, $parameter = null) use($app) {
-
-            include_once "classes/{$controller}.php";
-            $classe = new $controller();
-            $retorno = call_user_func_array(array($classe, "get_" . $action), array($parameter));
-            echo '{"result":' . json_encode($retorno) . '}';
-        });
-
-//POST não possui parâmetros na URL, e sim na requisição
-$app->post('/:controller/:action', function ($controller, $action) use ($app) {
-
-            $request = json_decode(\Slim\Slim::getInstance()->request()->getBody());
-            include_once "classes/{$controller}.php";
-            $classe = new $controller();
-            $retorno = call_user_func_array(array($classe, "post_" . $action), array($request));
-             echo '{"result":' . json_encode($retorno) . '}';
-        });
-*/
 
 $app->run();
