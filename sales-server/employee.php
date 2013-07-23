edffadb02e17c2fc7cfe1225eb7d5cb0b4b66bb8
danielps <danieljfa@gmail.com>
2013-07-23 09:34:24 -0300

    Funcionario com data

--- sales-server/employee.php
+++ sales-server/employee.php
@@ -10,7 +10,8 @@ $app->get("/employees",function (){
 
 $app->get("/employee/:id",function ($id){
 
-	$sql = "SELECT EmployeeID,FirstName,LastName,HomePhone FROM employees WHERE EmployeeID=?";
+	//DATE_FORMAT( `date` , '%d/%c/%Y %H:%i:%s' ) AS `date`
+	$sql = "SELECT EmployeeID,FirstName,LastName,HomePhone,DATE_FORMAT(BirthDate,'%d/%c/%Y') as BirthDate FROM employees WHERE EmployeeID=?";
 	$stmt = DB::prepare($sql);
 	$stmt->execute(array($id));
 	formatJson($stmt->fetch());
@@ -21,24 +22,26 @@ $app->post("/employee/",function (){
 	$data =json_decode(\Slim\Slim::getInstance()->request()->getBody());
 
 	if ($data->EmployeeID!=0){
-		$sql = "UPDATE employees SET FirstName=?,LastName=?,HomePhone=? WHERE EmployeeID=?";
+		$sql = "UPDATE employees SET FirstName=?,LastName=?,HomePhone=?,BirthDate=? WHERE EmployeeID=?";
 		$stmt = DB::prepare($sql);
 		$stmt->execute(array(
 			$data->FirstName,
 			$data->LastName,
 			$data->HomePhone,
+			DB::dateToMySql($data->BirthDate),
 			$data->EmployeeID
 			)
 		);
 	}
 	else
 	{
-		$sql = "INSERT INTO employees (FirstName,LastName,HomePhone)  VALUES (?,?,?)";
+		$sql = "INSERT INTO employees (FirstName,LastName,HomePhone,BirthDate)  VALUES (?,?,?,?)";
 		$stmt = DB::prepare($sql);
 		$stmt->execute(array(
 			$data->FirstName,
 			$data->LastName,
-			$data->HomePhone
+			$data->HomePhone,
+			DB::dateToMySql($data->BirthDate)
 			)
 		);
 		$data->EmployeeID = DB::lastInsertId();
